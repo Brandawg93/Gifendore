0335d44fa5a028e10629551277b3cd3f410b2e99
Brandon McFarlin <brandon.mcfarlin@yahoo.com>
2019-03-07 11:15:18 -0500

    fix crosspost bug

--- gifendore.py
+++ gifendore.py
@@ -158,8 +158,13 @@ async def process_inbox_item(item, comment, submission):
 
 	elif 'v.redd.it' in submission.url:
 		vid_name = submission.id
-		media = submission.secure_media
-		cross = submission.crosspost_parent_list
+		media = None
+		if hasattr(submission, 'secure_media'):
+			media = submission.secure_media
+		cross = None
+		if hasattr(submission, 'crosspost_parent_list'):
+			cross = submission.crosspost_parent_list
+
 		if media is not None and 'reddit_video' in media and 'fallback_url' in media['reddit_video']:
 			vid_url = media['reddit_video']['fallback_url']
 		elif cross is not None and len(cross) > 0 and 'secure_media' in cross[0] and 'reddit_video' in cross[0]['secure_media'] and 'fallback_url' in cross[0]['secure_media']['reddit_video']:
