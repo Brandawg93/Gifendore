f6b961dccc5070a89f42354fb6c4856a77124b41
Brandon McFarlin <brandon.mcfarlin@yahoo.com>
2019-05-02 19:00:07 -0400

    add album support for imgur

--- core/hosts/imgur.py
+++ core/hosts/imgur.py
@@ -1,4 +1,4 @@
-import requests, constants
+import requests, constants, re
 from core.exceptions import InvalidURLError, UploadError
 from .base import BaseHost
 from base64 import b64encode
@@ -6,20 +6,26 @@ from io import BytesIO
 
 class ImgurHost(BaseHost):
 	def __init__(self):
-		super().__init__('i.imgur', regex=r'http(s*)://i\.imgur\.com/(.*?)\.')
+		super().__init__('imgur', regex=r'http(s*)://i\.imgur\.com/(.*?)\.')
 
 	async def get_details(self, url):
+		if 'gallery' in url:
+			self.regex = re.compile(r'http(s*)://imgur\.com/gallery/(.*)', re.I)
 		self.name = self.regex.findall(url)[0][1]
 		if self.name is None:
 			raise InvalidURLError('Imgur url not found')
 		headers = {'Authorization': 'Client-ID {}'.format(constants.IMGUR_CLIENT_ID)}
-		imgur_response = requests.get('https://api.imgur.com/3/image/{}'.format(self.name), headers=headers)
+		endpoint = 'album/{}/images'.format(self.name) if 'gallery' in url else 'image/{}'.format(self.name)
+		imgur_response = requests.get('https://api.imgur.com/3/{}'.format(endpoint), headers=headers)
 		imgur_response.raise_for_status()
 		imgur_json = imgur_response.json()
-		if 'mp4' in imgur_json['data']:
-			self.vid_url = imgur_json['data']['mp4']
-		elif 'link' in imgur_json['data']:
-			self.gif_url = imgur_json['data']['link']
+		data = imgur_json['data']
+		if isinstance(data, list):
+			data = data[0]
+		if 'mp4' in data:
+			self.vid_url = data['mp4']
+		elif 'link' in data:
+			self.gif_url = data['link']
 		else:
 			raise InvalidURLError('Imgur url not found')
 
