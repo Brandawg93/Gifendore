133a1803d8e40aad7861c305e0dfe453a4e569b1
brandawg93 <brandawg93bot@gmail.com>
2019-09-17 11:53:13 -0400

    use heroku redis

--- core/memory.py
+++ core/memory.py
@@ -1,11 +1,15 @@
+import redis, constants
+
 class Memory:
 	def __init__(self):
-		self.memory = {}
+#		self.memory = {}
+		self.r = redis.from_url(constants.REDIS_URL)
 
 	def add(self, id, url, seconds=0):
 		try:
 			if not self.exists(id, seconds=seconds):
-				self.memory['{}-{}'.format(id, seconds)] = url
+#				self.memory['{}-{}'.format(id, seconds)] = url
+				self.r.set('{}-{}'.format(id, seconds), url)
 				return True
 			else:
 				return False
@@ -14,7 +18,8 @@ class Memory:
 
 	def remove(self, id, seconds=0):
 		try:
-			del self.memory['{}-{}'.format(id, seconds)]
+#			del self.memory['{}-{}'.format(id, seconds)]
+			self.r.delete('{}-{}'.format(id, seconds))
 			return True
 		except:
 			return False
@@ -22,7 +27,8 @@ class Memory:
 	def get(self, id, seconds=0):
 		try:
 			if self.exists(id, seconds=seconds):
-				return self.memory['{}-{}'.format(id, seconds)]
+#				return self.memory['{}-{}'.format(id, seconds)]
+				return self.r.get('{}-{}'.format(id, seconds))
 			else:
 				return None
 		except:
@@ -30,6 +36,7 @@ class Memory:
 
 	def exists(self, id, seconds=0):
 		try:
-			return '{}-{}'.format(id, seconds) in self.memory.keys()
+#			return '{}-{}'.format(id, seconds) in self.memory.keys()
+			return self.r.exists('{}-{}'.format(id, seconds))
 		except:
 			return False
