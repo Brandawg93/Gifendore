380f62c9ec4880cee218086a384debb9c1221dc5
brandawg93 <brandonmcfarlin1016@gmail.com>
2019-10-22 15:05:09 -0400

    support more youtube formats

--- core/hosts/youtube.py
+++ core/hosts/youtube.py
@@ -4,14 +4,15 @@ from core.exceptions import InvalidURLError
 
 class YoutubeHost(BaseHost):
 	def __init__(self):
-		super().__init__('youtube', regex=r'https://www\.youtube\.com/watch(?:/)*\?v=(.+)')
+		super().__init__('youtu', regex=r'(https://www\.youtube\.com/watch(?:/)*\?v=(.+))|(https://youtu\.be/(.+))')
 
 	async def get_details(self, url):
 		try:
-			self.name = self.regex.findall(url)[0]
+			lst = self.regex.findall(url)[0]
+			self.name = [x for x in lst if x is not '' and 'https://' not in x][0]
 		except IndexError:
 			raise InvalidURLError('youtube url not found')
-		if self.name is None:
+		if self.name is None or self.name == '':
 			raise InvalidURLError('youtube url not found')
 		self.img_url = "https://img.youtube.com/vi/{}/maxresdefault.jpg".format(self.name)
 		return self.get_info()
