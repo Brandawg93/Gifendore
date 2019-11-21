641b943c56dae913d69da39ce0dd2e87b2b7e678
Brandon McFarlin <brandonmcfarlin1016@gmail.com>
2019-11-21 08:52:29 -0500

    redis will timeout if needed

--- core/memory/base.py
+++ core/memory/base.py
@@ -1,6 +1,7 @@
 import constants
 import redis
 import logging
+from decorators import timeout
 
 logger = logging.getLogger("gifendore")
 
@@ -9,6 +10,7 @@ class BaseMemory:
     def __init__(self):
         self.r = redis.from_url(constants.REDIS_URL, decode_responses=True)
 
+    @timeout(3)
     def redis_add(self, key, value):
         try:
             # ex: expire time in seconds
@@ -16,20 +18,22 @@ class BaseMemory:
             self.r.set(key, value, ex=None, nx=False)
             return True
         except Exception as e:
-            logger.debug(e)
+            logger.warning(e)
             return False
 
+    @timeout(3)
     def redis_delete(self, key):
         try:
             self.r.delete(key)
             return True
         except Exception as e:
-            logger.debug(e)
+            logger.warning(e)
             return False
 
+    @timeout(3)
     def redis_get(self, key):
         try:
             return self.r.get(key)
         except Exception as e:
-            logger.debug(e)
+            logger.warning(e)
             return None
--- decorators.py
+++ decorators.py
@@ -1,6 +1,7 @@
 import functools
 import logging
 import time
+import signal
 
 logger = logging.getLogger("gifendore")
 
@@ -49,3 +50,22 @@ def async_timer(func):
         return value
 
     return wrapper_timer
+
+
+def timeout(seconds):
+    def decorated(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(f"Function {func.__name__!r} timed out")
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return functools.wraps(func)(wrapper)
+
+    return decorated
