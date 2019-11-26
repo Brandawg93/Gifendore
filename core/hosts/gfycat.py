7bf1d5e0c595b43e5feff0ad5c412b2861e11075
Brandon McFarlin <brandonmcfarlin1016@gmail.com>
2019-11-26 17:43:16 -0500

    Update gfycat.py

--- core/hosts/gfycat.py
+++ core/hosts/gfycat.py
@@ -30,5 +30,5 @@ class GfycatHost(BaseHost):
                 else:
                     raise InvalidURLError('gfycat url not found')
             return self.get_info()
-        except Exception as e:
+        except:
             raise InvalidURLError('gfycat url not found')
