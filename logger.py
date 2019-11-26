8f270a420b1c1a16f18b20ad7b0df550149b1b16
Brandon McFarlin <brandonmcfarlin1016@gmail.com>
2019-11-26 17:56:08 -0500

    stop sending debug

--- logger.py
+++ logger.py
@@ -2,36 +2,28 @@ import logging
 from sentry_sdk import configure_scope, capture_exception
 
 
+def _set_scope(inbox_item, level='error'):
+    with configure_scope() as scope:
+        if inbox_item:
+            scope.set_level(level)
+            scope.set_user({"username": inbox_item.item.author.name, "id": inbox_item.item.author.id})
+            scope.set_extra("subreddit", inbox_item.item.submission.subreddit.display_name)
+            scope.set_extra("submission", inbox_item.submission.shortlink)
+            scope.set_extra("mention", inbox_item.item.body)
+
+
 class MyLogger(logging.Logger):
     def __init__(self, name, level=logging.NOTSET):
         super(MyLogger, self).__init__(name, level)
 
     def warning(self, msg, *args, inbox_item=None, **kwargs):
-        with configure_scope() as scope:
-            scope.set_level('warning')
-            if inbox_item:
-                scope.set_user({"username": inbox_item.item.author.name, "id": inbox_item.item.author.id})
-                scope.set_extra("subreddit", inbox_item.item.submission.subreddit.display_name)
-                scope.set_extra("submission", inbox_item.submission.shortlink)
-                scope.set_extra("mention", inbox_item.item.body)
-            capture_exception(msg)
+        _set_scope(inbox_item, 'warning')
+        capture_exception(msg)
 
         return super(MyLogger, self).warning(msg, *args, **kwargs)
 
-    def debug(self, msg, *args, **kwargs):
-        with configure_scope() as scope:
-            scope.set_level('debug')
-            capture_exception(msg)
-
-        return super(MyLogger, self).debug(msg, *args, **kwargs)
-
     def exception(self, msg, *args, exc_info=True, inbox_item=None, **kwargs):
-        with configure_scope() as scope:
-            if inbox_item:
-                scope.set_user({"username": inbox_item.item.author.name, "id": inbox_item.item.author.id})
-                scope.set_extra("subreddit", inbox_item.item.submission.subreddit.display_name)
-                scope.set_extra("submission", inbox_item.submission.shortlink)
-                scope.set_extra("mention", inbox_item.item.body)
-            capture_exception(msg)
+        _set_scope(inbox_item)
+        capture_exception(msg)
 
         return super(MyLogger, self).exception(msg, *args, exc_info=exc_info, **kwargs)
