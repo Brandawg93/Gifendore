9cc6e7ccf80bb5ccbc8354e4d08b6e5d1c6a14c2
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2021-04-13 12:42:26 -0400

    test redis connection

---
+++ tests/memory/test_base.py
@@ -0,0 +1,9 @@
+import constants
+import redis
+
+
+def test_redis_connection():
+    """Connects to redis."""
+    r = redis.from_url(constants.REDIS_URL, decode_responses=True, socket_connect_timeout=1)
+    r.ping()
+    assert r
