05f1bd89efa408e73dc5efb82757dbb0622af4cb
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2022-08-28 14:37:01 -0400

    remove keen

--- services.py
+++ services.py
@@ -1,29 +1,27 @@
 import constants
-import sentry_sdk
-import keen
 import logging
 from sentry_sdk.integrations.redis import RedisIntegration
 from urllib.parse import urlparse
 from core.config import config
+from pymongo import MongoClient
 
 logger = logging.getLogger("gifendore")
-if not config.is_testing_environ:
-	sentry_sdk.init(dsn=constants.SENTRY_DSN, environment='production', integrations=[RedisIntegration()])
-
 
 def log_event(name, item, url=None):
-	"""Log event to keen."""
+	"""Log event to db."""
 	try:
 		if not config.is_testing_environ:
 			if url:
 				split_url = urlparse(url)
 				url = '{}://{}'.format(split_url.scheme, split_url.netloc)
-
-			keen.add_event(name, {
+			
+			client = MongoClient(constants.MONGODB_URL)
+			db = client.analytics
+			db[name].insert_one({
 				"user": item.author.name,
 				"subreddit": item.submission.subreddit.display_name,
 				"host": url
-			})
-			logger.debug("sent {} event to keen".format(name))
+			})			
+			logger.debug("sent {} event to db".format(name))
 	except Exception as e:
-		logger.debug("Could not send to keen", exc_info=e)
+		logger.debug("Could not send to db", exc_info=e)
