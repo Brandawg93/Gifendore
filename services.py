c964eceb22998efffbe8d70df2e4037d0b14cebe
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2022-08-28 15:56:52 -0400

    replace redis

--- services.py
+++ services.py
@@ -10,18 +10,17 @@ logger = logging.getLogger("gifendore")
 def log_event(name, item, url=None):
 	"""Log event to db."""
 	try:
-		if not config.is_testing_environ:
-			if url:
-				split_url = urlparse(url)
-				url = '{}://{}'.format(split_url.scheme, split_url.netloc)
-			
-			client = MongoClient(constants.MONGODB_URL)
-			db = client.analytics
-			db[name].insert_one({
-				"user": item.author.name,
-				"subreddit": item.submission.subreddit.display_name,
-				"host": url
-			})			
-			logger.debug("sent {} event to db".format(name))
+		if url:
+			split_url = urlparse(url)
+			url = '{}://{}'.format(split_url.scheme, split_url.netloc)
+		
+		client = MongoClient(constants.MONGODB_URL)
+		db = client.analytics
+		db[name].insert_one({
+			"user": item.author.name,
+			"subreddit": item.submission.subreddit.display_name,
+			"host": url
+		})			
+		logger.debug("sent {} event to db".format(name))
 	except Exception as e:
 		logger.debug("Could not send to db", exc_info=e)
